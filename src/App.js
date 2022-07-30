4116145699110a3efaa969a8198a194b5887137d
Hamed <hamed.jenabi@gmail.com>
2022-07-30 16:38:38 +0200

    add calculator component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import logo from "./logo.svg";
-import "./App.css";
+import "./App.scss";
 import CalculatorComponent from "./Component/Calculator/CalculatorComponent";
 function App() {
   return (
     <div className="App">
-      <head>
-        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
-      </head>
       <CalculatorComponent />
     </div>
   );
--- src/Component/Calculator/CalculatorComponent.js
+++ src/Component/Calculator/CalculatorComponent.js
@@ -1,7 +1,26 @@
 import React, { useEffect, useState } from "react";
 import styles from "./CalculatorComponent.module.scss";
+import Confetti from "./Confetti";
+const mockData = {
+  route: [
+    {
+      lat: 48.215739883024661,
+      lng: 16.370281800013927,
+    },
+    {
+      lat: 48.215739883024661,
+      lng: 17.370281800013927,
+    },
+  ],
+  merchant: {
+    co2SavedKg: 200.8,
+  },
+  customer: {
+    co2SavedKg: 1.28,
+  },
+};
 
-const ProgressBar = ({ value, duration = 20 }) => {
+const Co2Display = ({ value, duration = 10 }) => {
   const [count, setCount] = useState(0);
   useEffect(() => {
     const timeout = setTimeout(() => {
@@ -13,33 +32,41 @@ const ProgressBar = ({ value, duration = 20 }) => {
     return () => clearTimeout(timeout);
   }, [count]);
   return (
-    <div
-      style={{
-        backgroundImage: `linear-gradient(to right, blue
-      ${count}%, white ${count}% 100%)`,
-      }}
-      className={styles.progressBar}
-    >
-      <p class="" size="lg" noSpacing>
-        {count}
-      </p>
+    <div className={styles.Co2Display}>
+      <p>{count}</p>
     </div>
   );
 };
 
 const CalculatorComponent = () => {
-  const fetchData = async () => {
-    const response = await fetch("https://api.github.com/users/octocat");
-    const data = await response.json();
-    console.log(data);
-  };
+  const [start, setStart] = useState(false);
+  const { route, merchant, customer } = mockData;
+  // useEffect(() => {
+  //   setInterval(() => {
+  //     setStart(!start);
+  //   }, 2000);
+  // }, []);
 
   return (
     <div style={{ width: "100%" }}>
-      <div style={{ display: "flex" }}>
-        <ProgressBar value={50} />
-        <ProgressBar value={50} />
+      <div className={styles.container}>
+        <div className={styles.displayWrapper}>
+          <p className={styles.title}>
+            The amount of CO&#178; You saved with this purchase
+          </p>
+          <Co2Display value={customer?.co2SavedKg} duration={120} />
+        </div>
+        <div className={styles.displayWrapper}>
+          <p className={styles.title}>
+            The amount of CO&#178; the shop saved the last month
+          </p>
+          <Co2Display value={merchant?.co2SavedKg} duration={5} />
+        </div>
       </div>
+      <div className={styles.mapWrapper} onClick={() => setStart(!start)}>
+        THIS IS THE MAP HERE
+      </div>
+      {start && <Confetti start={start} />}
     </div>
   );
 };
